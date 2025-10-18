bfa30bbbce2b4f6ebc6c6091160fbd89ca3a064e
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-10-18 11:15:28 +0530

    [ADDED]: shubham This code for performing addition of two number

---
+++ AdditionDemo.java
@@ -0,0 +1,15 @@
+class AdditionDemo{
+
+	public static void main(String args[]){
+
+		int a = 10;
+		int b = 20;
+		int c = 0;
+
+		c = a+b;
+		
+		System.out.println(" Addition ="+c);
+			
+	}
+
+}
