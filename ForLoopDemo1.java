cdf9bd35e609dc362a885fd89382472dbcf7baeb
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-12-19 19:23:22 +0530

    [ADDED]: shubham This code for For Loop Demo

---
+++ ForLoopDemo1.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+class ForLoopDemo1{
+
+	public static void main(String args[]){
+	
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the no:");		
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			
+			System.out.println("James Gosling ");
+		}
+		
+
+	}
+}
